7b8d6f5d2c4ad48db1a4b825c1b946c2b8a5e947
ximitc <844801105@qq.com>
2016-04-14 09:25:38 +0800

    Create daemon.py

---
+++ daemon.py
@@ -0,0 +1,43 @@
+import sys, os
+
+def main():
+    """ An example daemon main routine; writes a datestamp to file
+        /tmp/daemon-log every 10 seconds.
+    """
+    import time
+    f = open("/tmp/daemon-log", "w")
+    while 1:
+        f.write('%s\n' % time.ctime(time.time()))
+        f.flush(  )
+        time.sleep(10)
+
+if __name__ == "__main__":
+    try:
+        pid = os.fork(  )
+        if pid > 0:
+            # Exit first parent
+            sys.exit(0)
+    except OSError, e:
+        print >>sys.stderr, "fork #1 failed: %d (%s)" % (
+            e.errno, e.strerror)
+        sys.exit(1)
+
+    # Decouple from parent environment
+    os.chdir("/")
+    os.setsid(  )
+    os.umask(0)
+
+    # Do second fork
+    try:
+        pid = os.fork(  )
+        if pid > 0:
+            # Exit from second parent; print eventual PID before exiting
+            print "Daemon PID %d" % pid
+            sys.exit(0)
+    except OSError, e:
+        print >>sys.stderr, "fork #2 failed: %d (%s)" % (
+            e.errno, e.strerror)
+        sys.exit(1)
+
+    # Start the daemon main loop
+    main()
